84cfd1b6897045133c031c045a37c07cde713178
Co-D-E-R <zh4223828@gmail.com>
2023-08-16 23:47:19 +0530

    bootstrap

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
 
 // mongoose.connect('mongodb://localhost:27017/yelp-camp',{
@@ -26,12 +27,13 @@ async function main() {
 
 const app = express();
 
-
+app.engine('ejs',ejsMate);
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride('_method'));
+
 
 app.get('/',(req,res) =>{
     res.render('home');
--- seeds/index.js
+++ seeds/index.js
@@ -42,9 +42,13 @@ const seeddb = async () => {
     await Campground.deleteMany({});
     for(let i=0;i<50;i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random()*20) +10;
         const camp = new Campground({
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image:'https://source.unsplash.com/collection/483251',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio doloribus possimus tempore voluptatibus delectus voluptatem omnis amet fuga veniam quo libero eius aliquid iure, porro ratione, quam dolorem similique. Repellat?',
+            price
         })
         await camp.save();
 
