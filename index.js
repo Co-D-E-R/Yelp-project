5c69dd69c3a123f10eeadea5e2ea7905eb6fffa1
Co-D-E-R <zh4223828@gmail.com>
2023-08-18 02:54:04 +0530

    error and validation

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const ejsMate = require('ejs-mate');
+const joi = require('joi');
 const Campground = require('./models/campground');
 
 // mongoose.connect('mongodb://localhost:27017/yelp-camp',{
@@ -39,43 +42,68 @@ app.get('/',(req,res) =>{
     res.render('home');
 })
 
-app.get('/campgrounds',async (req,res) =>{
+app.get('/campgrounds',catchAsync(async (req,res) =>{
    const campgrounds = await Campground.find({});
    res.render('campgrounds/index',{ campgrounds })
-})
+}))
 
 app.get('/campgrounds/new',(req,res) =>{
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds',async(req,res) =>{
+app.post('/campgrounds',catchAsync(async(req,res,next) =>{
+    // if(!req.body.campground) throw new ExpressError('Invalid Data',400);
+    const campgroundSchema = joi.object({
+        campground: joi.object({
+            title: joi.string().required(),
+            price: joi.number().required().min(0),
+            image: joi.string().required(),
+            location: joi.string().required(),
+            description: joi.string().required()
+        }).required()
+    })
+    const { error }= campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
+    console.log(result);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.get('/campgrounds/:id',async(req,res) =>{
+app.get('/campgrounds/:id',catchAsync(async(req,res) =>{
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show',{ campground });
-})
+}))
 
-app.get('/campgrounds/:id/edit',async(req,res) =>{
+app.get('/campgrounds/:id/edit',catchAsync(async(req,res) =>{
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit',{ campground });
-})
+}))
 
-app.put('/campgrounds/:id', async(req,res) =>{
+app.put('/campgrounds/:id', catchAsync(async(req,res) =>{
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`);
-})
+}))
 
-app.delete('/campgrounds/:id', async(req,res) =>{
+app.delete('/campgrounds/:id', catchAsync(async(req,res) =>{
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}));
+
+app.all('*',(req,res,next)=>{
+    next(new ExpressError('Page Not Found',404))
 })
 
+app.use((err,req,res,next) =>{
+    const { statusCode = 500} = err;
+    if(!err.message) err.message= 'Oh No,Something Went Worng'
+    res.status(statusCode).render('error',{ err });
+})
 
 app.listen(3000, () =>{
     console.log("Listening to port 3000!!");
