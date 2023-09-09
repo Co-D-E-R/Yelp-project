ac557f55ddc5292e9471e4ab3a3b2b0f37b99181
Co-D-E-R <zh4223828@gmail.com>
2023-09-09 16:08:32 +0530

    Review section is added

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const ejsMate = require('ejs-mate');
-const joi = require('joi');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 // mongoose.connect('mongodb://localhost:27017/yelp-camp',{
 //     useNewUrlParser:true,
@@ -38,6 +39,26 @@ app.use(express.urlencoded({extended : true}));
 app.use(methodOverride('_method'));
 
 
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+const validateReview= (req,res,next) =>{
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
 app.get('/',(req,res) =>{
     res.render('home');
 })
@@ -51,30 +72,15 @@ app.get('/campgrounds/new',(req,res) =>{
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds',catchAsync(async(req,res,next) =>{
+app.post('/campgrounds',validateCampground, catchAsync(async(req,res,next) =>{
     // if(!req.body.campground) throw new ExpressError('Invalid Data',400);
-    const campgroundSchema = joi.object({
-        campground: joi.object({
-            title: joi.string().required(),
-            price: joi.number().required().min(0),
-            image: joi.string().required(),
-            location: joi.string().required(),
-            description: joi.string().required()
-        }).required()
-    })
-    const { error }= campgroundSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }
-    console.log(result);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 app.get('/campgrounds/:id',catchAsync(async(req,res) =>{
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show',{ campground });
 }))
 
@@ -83,7 +89,7 @@ app.get('/campgrounds/:id/edit',catchAsync(async(req,res) =>{
     res.render('campgrounds/edit',{ campground });
 }))
 
-app.put('/campgrounds/:id', catchAsync(async(req,res) =>{
+app.put('/campgrounds/:id', validateCampground, catchAsync(async(req,res) =>{
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`);
@@ -94,6 +100,22 @@ app.delete('/campgrounds/:id', catchAsync(async(req,res) =>{
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }));
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)=>{
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId',catchAsync(async(req,res)=>{
+    const{ id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id,{$pull : { reviews : reviewId}})
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
 
 app.all('*',(req,res,next)=>{
     next(new ExpressError('Page Not Found',404))
