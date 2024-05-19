cf5480e7fa51c3acde9ab95babb77d87905444f0
Co-D-E-R <zh4223828@gmail.com>
2024-05-19 21:26:26 +0530

    error fix

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params:{
-        folder: 'YelpCamp',
+        folder: 'YelpCAmp',
         allowedFormats: ['jpeg', 'png','jpg' ]
     }
 });
